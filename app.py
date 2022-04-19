0117a57a8493512891145c745d12096fbe9341f9
Jacob Camarillo <jcamarillo@ucsd.edu>
2022-04-18 20:01:42 -0700

    baseline for new pages

--- app.py
+++ app.py
@@ -7,30 +7,35 @@ from pyramid.response import FileResponse
 def index_page(req):
    return FileResponse("index.html")
 
-# def music_page(req):
-#    return FileResponse("music.html")
+def product_page(req):
+   return FileResponse("product.html")
+
+def kvp_page(req):
+   return FileResponse("kvp.html")
  
 #Line below tells executor to start from here
 if __name__ == '__main__':
    with Configurator() as config:
        
        # Create a route called home
-       config.add_route('home', '/')
-       
+      config.add_route('home', '/')      
        # Bind the view (defined by index_page) to the route named ‘home’
-       config.add_view(index_page, route_name='home')
+      config.add_view(index_page, route_name='home')
       
-      #  # Create a route that handles server HTTP requests at: /music
-      #  config.add_route('music', '/music')
-       
-      #  # Binds the function get_music route and returns music page
-      #  config.add_view(music_page, route_name='music')
+      #  Create a route that handles server HTTP requests at: /product
+      config.add_route('product', '/product')       
+      #  Binds the function get_music route and returns product page
+      config.add_view(product_page, route_name='product')
+
+      # Create route that handles server requests at /kvp
+      config.add_route('kvp', '/kvp' )
+      config.add_view(kvp_page, route_name = 'kvp')
  
        # Add a static view
-       config.add_static_view(name='/', path='./public', cache_max_age=3600)
+      config.add_static_view(name='/', path='./public', cache_max_age=3600)
       
        # Create an app with the configuration specified above       
-       app = config.make_wsgi_app()
+      app = config.make_wsgi_app()
        
    # start the server on port 6543
    print("Server started on port 6543")
